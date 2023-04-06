671fbd15537fb312005f6b8ee83c2846c4d7520d
Javier Lucero <javimcl1986@gmail.com>
2023-04-06 12:06:24 -0500

    chance version

--- src/test/java/com/example/SpringSampleAppApplicationTests.java
+++ src/test/java/com/example/SpringSampleAppApplicationTests.java
@@ -1,16 +1,11 @@
 package com.example;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootApplication
 public class SpringSampleAppApplicationTests {
-
-	@Test
-	public void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleAppApplicationTests.class, args);
 	}
-
 }
